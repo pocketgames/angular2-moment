df2d8c82435fd7d7ae338278ff746b043785aa7c
Mogor Imre <imogor@pocketgames.eu>
2017-03-10 17:29:03 +0100

    Add AddPipe and SubtractPipe exports to index.ts part 2

--- src/moment.module.ts
+++ src/moment.module.ts
@@ -7,9 +7,11 @@ import { DurationPipe } from './duration.pipe';
 import { FromUnixPipe } from './from-unix.pipe';
 import { TimeAgoPipe } from './time-ago.pipe';
 import { UtcPipe } from './utc.pipe';
+import {AddPipe} from "./add.pipe";
+import { SubtractPipe } from './subtract.pipe';
 
 const ANGULAR_MOMENT_PIPES = [CalendarPipe, DateFormatPipe, DifferencePipe, DurationPipe, FromUnixPipe, TimeAgoPipe,
-   UtcPipe];
+   UtcPipe, AddPipe, SubtractPipe];
 
 @NgModule({
   declarations: ANGULAR_MOMENT_PIPES,
